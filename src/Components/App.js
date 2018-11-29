8cb32278ee8b450210a481b45cc816b93f00461a
Kristin <kristinjoypeterson@gmail.com>
2018-11-28 18:37:04 -0700

    This add the gameroom card and the json package for the clock

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from "react";
 import AppBar from "./Layouts/AppBar";
+import Paper from "./GameRoom/Paper"
 
 export default class extends Component {
   render() {
     return (
       <Fragment>
         <AppBar />
+        <Paper />
       </Fragment>
     );
   }
