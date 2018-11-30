6cc55907e5d70711bf91dfe840c7f01f5483ac4d
Kristin <kristinjoypeterson@gmail.com>
2018-11-29 19:30:24 -0700

    This adds the challenge list view

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,13 @@
 import React, { Component, Fragment } from "react";
 import AppBar from "./Layouts/AppBar";
-import LobbyPaper from "./LobbyScreen/LobbyPaper"
+import ChallengePaper from "./ChallengeList/ChallengePaper"
 
 export default class extends Component {
   render() {
     return (
       <Fragment>
         <AppBar />
-        <LobbyPaper />
+        <ChallengePaper />
       </Fragment>
     );
   }
