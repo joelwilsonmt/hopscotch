790317d52261e9f08bdb84d6852492681a8842c8
Joel <joelwilsonmt@gmail.com>
2018-11-29 19:23:07 -0700

    created circut schema

---
+++ models/circuit.js
@@ -0,0 +1,24 @@
+var mongoose = require('./../config/db-config');
+
+CircuitSchema  = mongoose.Schema(
+    {
+      users:[Array],
+      challenges:{
+        object_gate: [String],
+        location_gate: [String],
+        id_users_completed: [Number]
+      }
+      time_started: Date,
+      time_completed: Date,
+      date_created: Date,
+      date_deleted: Date,
+      archived: Boolean,
+      delete_in_ten_days: {
+        delete: Boolean,
+        start_delete: Date
+        }
+    },
+    { collection: 'circuits' });
+Circuit = mongoose.model('Circuit', CircuitSchema);
+
+module.exports = Circuit;
