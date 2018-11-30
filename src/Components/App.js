20e284cdb42fc932847157e55bc033e577140c55
Kristin <kristinjoypeterson@gmail.com>
2018-11-29 18:42:29 -0700

    This add the lobby room screen and changes the route for the timer

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,13 @@
 import React, { Component, Fragment } from "react";
 import AppBar from "./Layouts/AppBar";
-import Paper from "./GameRoom/Paper"
+import LobbyPaper from "./LobbyScreen/LobbyPaper"
 
 export default class extends Component {
   render() {
     return (
       <Fragment>
         <AppBar />
-        <Paper />
+        <LobbyPaper />
       </Fragment>
     );
   }
