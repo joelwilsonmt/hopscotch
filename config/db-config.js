e669aab0e8e907eef6694b2d49c7789fcb675fc8
Joel <joelwilsonmt@gmail.com>
2018-12-06 22:52:31 -0700

    tried .env file mightve changed other stuff

--- config/db-config.js
+++ config/db-config.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect("mongodb://circuitbreaker:Thisisagoodpassword!1@ds119164.mlab.com:19164/circuitbreaker");
+
+mongoose.connect('mongodb://circuitbreaker:Thisisagoodpassword!1@ds119164.mlab.com:19164/circuitbreaker');
 
 
 module.exports = mongoose;
