89e15273338d892c66aea80e7e5364bd6598c57d
jiguchi <jamie.iguchi@gmail.com>
2018-12-20 01:50:43 -0700

    Added label match search to submitChallenge route. Amended request object in Camera.js.

--- routes/submitChallenge.js
+++ routes/submitChallenge.js
@@ -63,7 +63,44 @@ function pictureIsValid(pictureFile) {
     MinConfidence: 50
   })
   .promise().then(function(res){
-    console.log(res);
+    var responseData = res;
+    console.log("AWS response (this is a JSON array) :", responseData);
+
+    // Converting AWS response (JSON array) to an array of the label objects
+    // (This is all very hacky...)
+    var labelObjectArray = [];
+    Object.keys(responseData.Labels).forEach(function(key){
+      var item = responseData.Labels[key];
+      labelObjectArray.push(item);
+    })
+    // console.log("An array of the Label objects: ", labelObjectArray);
+
+
+    // Converting array of label objects to array of just the names of the labels
+    // (Still very hacky. I'm sure there's a way more elegant way to do this...)
+    var justTheLabels = [];
+    Object.keys(labelObjectArray).forEach(function(key) {
+      var val = labelObjectArray[key]["Name"];
+      justTheLabels.push(val);
+    });
+    console.log("Array of just the AWS labels only: ", justTheLabels);
+
+
+    // Loop through this array of names to find match with object_to_check
+    var checkWord = "Face"; // Need to fix this to use check_word from front end request object
+    var found = false;
+    for (var i = 0; i < justTheLabels.length; i++) {
+      if (checkWord === justTheLabels[i]) {
+        found = true;
+        break;
+      }
+    }
+    if (found) {
+      console.log("Congrats! There is a match with", checkWord, "in the following list of acceptable words", justTheLabels);
+    } else {
+      console.log("Sorry, there is no match with", checkWord, "in the following list of acceptable words", justTheLabels);
+    }
+
   })
   .catch(function(err){
     console.error(err);
