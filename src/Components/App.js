23682b2a579e6c4000eb076d89dced1f33ed0a75
Joel <joelwilsonmt@gmail.com>
2019-01-02 15:41:10 -0700

    added gif bg

--- src/Components/App.js
+++ src/Components/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
   }
 
   componentDidMount(){
-    document.body.style.backgroundImage = `url(${Background})`;
+    //document.body.style.backgroundImage = `url(${Background})`;
   }
 
   render() {
