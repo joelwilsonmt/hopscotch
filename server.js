85c056e04e49c076e4befa14ee3277c34efc5f4b
Kristin <kristinjoypeterson@gmail.com>
2019-01-05 10:31:13 -0700

    Working on deployment

--- server.js
+++ server.js
@@ -85,7 +85,13 @@ io.on('connection', function(client){
       console.log('User disconnected!');
     });
 });//closes io
+let port;
+if(!process.env.PRODUCTION){
+  port = 3001;
+}else{
+  port = process.env.PORT
+}
 
-http.listen(3001, function(){
-  console.log('listening on *:3001');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
